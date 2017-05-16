f4e1cd0119254ee23989fc5b6ba85e22f6159ce5
Matteo <hpisdnhs@gmail.com>
2017-05-15 20:53:36 -0500

    Button 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,35 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
+/* ADDED JS */
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* ADDED JS */
+
 (function($) {
 
 	skel.breakpoints({
