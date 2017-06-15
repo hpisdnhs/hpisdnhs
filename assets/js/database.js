364338ca23289defe45bb5bbcf6529fc78afdb69
Matteo Cordray <matteocordray@iMac.local>
2017-06-15 18:49:24 -0500

    Firebase

--- assets/js/database.js
+++ assets/js/database.js
@@ -4,7 +4,7 @@ var config = {
   apiKey: "AIzaSyCID96dTRx69zOeZNfOH81xQQIUD6jvPYs",
   authDomain: "hpisd-nhs.firebaseapp.com",
   databaseURL: "https://hpisdnhs.firebaseio.com",
-  storageBucket: "bucket.appspot.com"
+  storageBucket: "hpisd-nhs.appspot.com"
 };
 firebase.initializeApp(config);
 
