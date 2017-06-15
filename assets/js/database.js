b4fd9b51f83d1af07cbf17ba6dea8b5f06118d16
Matteo Cordray <matteocordray@iMac.local>
2017-06-15 18:39:06 -0500

    Tried Firebase

---
+++ assets/js/database.js
@@ -0,0 +1,12 @@
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+var config = {
+  apiKey: "AIzaSyCID96dTRx69zOeZNfOH81xQQIUD6jvPYs",
+  authDomain: "hpisd-nhs.firebaseapp.com",
+  databaseURL: "https://hpisdnhs.firebaseio.com",
+  storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
